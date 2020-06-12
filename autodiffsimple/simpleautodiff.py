9599b59a7ae2b7c15c7671c6760725793bb410ff
Tristan Swedish <tristan.swedish@gmail.com>
2020-06-12 10:42:08 -0400

    Update simpleautodiff.py

--- autodiffsimple/simpleautodiff.py
+++ autodiffsimple/simpleautodiff.py
@@ -1,3 +1,19 @@
+#simpleautodiff - example autodiff for forward and reverse modes in python
+#Copyright (C) 2020 Tristan Swedish
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <https://www.gnu.org/licenses/>. 
+
 # needed for sin/cos/log
 # see https://github.com/mitmedialab/cameracultureblogs/blob/master/autodiffsimple/autodiffsimple.md
 import math
